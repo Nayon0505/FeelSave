ae763a806801ac462141224c20f481080db4eb1e
Nayon0505 <nxyon.lenz@gmail.com>
2025-03-11 12:10:03 +0100

    refactoring von strings

--- app/src/main/java/com/example/feelsave/InfoDialogHelper.java
+++ app/src/main/java/com/example/feelsave/InfoDialogHelper.java
@@ -2,6 +2,10 @@ package com.example.feelsave;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AlertDialog;
 
 public class InfoDialogHelper {
@@ -9,20 +13,18 @@ public class InfoDialogHelper {
     public static void showAppInfo(Context context) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
+        TextView messageTextView = new TextView(context);
+        messageTextView.setText(context.getString(R.string.infodialog_title) +
+                context.getString(R.string.infodialog1) +
+                context.getString(R.string.feelsaveinfolink));
+
+        messageTextView.setAutoLinkMask(Linkify.WEB_URLS);
+        messageTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        messageTextView.setPadding(50, 20, 50, 20);
+
         builder.setTitle("Wie funktioniert FeelSave?")
-                .setMessage("Mit einem Knopfdruck sicherer.\n\n" +
-                        "1. Erlaube FeelSave alle nötigen Zugriffe!\n\n" +
-                        "2. Wähle Notfallkontakte in den Einstellungen aus\n\n" +
-                        "3. Personalisiere deine Notfallnachricht (oder lass sie so :))\n\n" +
-                        "4. Halte den Button wenn du dich unsicher fühlst, nach ein Paar Sekunden geht der SaveMode an\n\n" +
-                        "5. Lässt du nun den Finger ein Paar Sekunden vom ab, werden deine Notfallkontakte alamiert und bekommen deinen Standort\n\n" +
-                        "6. Für mehr Infos, besuche unsere Infoseite\n\n")
-                .setPositiveButton("Verstanden.", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
+                .setView(messageTextView) // Setze den TextView in den Dialog
+                .setPositiveButton("Verstanden.", (dialog, which) -> dialog.dismiss());
 
         AlertDialog dialog = builder.create();
         dialog.show();
